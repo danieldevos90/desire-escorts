ac8d94e4f1cb9e3591ad42c798f7a49c170e9cde
Daniel de Vos <your-email@example.com>
2025-08-08 16:56:17 +0200

    fix(news routes): remove duplicate default export

--- backend/src/api/news/routes/news.ts
+++ backend/src/api/news/routes/news.ts
@@ -5,11 +5,4 @@ export default {
   ],
 };
 
-export default {
-  routes: [
-    { method: 'GET', path: '/news', handler: 'api::news.news.find', config: { policies: ['global::site-scope'] } },
-    { method: 'GET', path: '/news/:id', handler: 'api::news.news.findOne', config: { policies: ['global::site-scope'] } },
-  ],
-};
-
 
